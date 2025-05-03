030fa41e3d1af000acd589e7806ac9e9684e9e6a
PanDaTech <66164680+PanDaDaTech@users.noreply.github.com>
2025-05-03 22:18:12 +0800

    修正 代码几处错误

--- osc/xrkms/xrkms.bat
+++ osc/xrkms/xrkms.bat
@@ -5,17 +5,9 @@ setlocal enabledelayedexpansion
 set ver=智能正版激活工具 V3.25.3.29
 title %ver%（请勿关闭此窗口）
 if exist "%systemdrive%\Windows\Setup\xrsysnokms.txt" exit
+if exist "%systemdrive%\Windows\Setup\pandasysnokms.txt" exit
 if exist "%SystemDrive%\wandrv\wall.exe" exit
 if exist wbox.exe set wbox="%~dp0wbox.exe"
-@rem if not exist "%SystemDrive%\WINDOWS\Setup\Set\api.exe" (
-@rem     if exist "%SystemDrive%\WINDOWS\OSConfig\osc.exe" (
-@rem         if exist "%SystemDrive%\wandrv\wall.exe" exit
-@rem     ) else (
-@rem         echo 检测到您未在潇然系统部署过程中运行，程序即将退出！！！
-@rem         timeout -t 5 2>nul || ping 127.0.0.1 -n 5 >nul
-@rem         exit
-@rem     )
-@rem )
 
 :ask
 cls
@@ -84,6 +76,7 @@ if %osver% EQU 2 (
 )
 
 if exist "%systemdrive%\Windows\Setup\xrsysentg.txt" set isentg=1
+if exist "%systemdrive%\Windows\Setup\pandasysentg.txt" set isentg=1
 
 if "%isentg%"=="1" call :windowsentg
 call :checkmsostate
@@ -95,54 +88,54 @@ if "%server%"=="" goto offline
 if not exist vlmcs.exe goto offline
 cls
 title %ver% - 网络测试（请勿关闭此窗口）
-echo 正在测试您的电脑是否能与激活服务器%server%连接...
+echo 正在测试您的电脑是否能与激活服务器 %server% 连接...
 vlmcs.exe -l 1 %server% 2>nul | find /i "successful" 1>nul 2>nul && (
-    echo 您的电脑能与激活服务器%server%连接，即将为您在线激活
+    echo 您的电脑能与激活服务器 %server% 连接，即将为您在线激活
     goto online
 ) || (
-    echo 您的电脑不能与激活服务器%server%连接，即将为您尝试另一个服务器
+    echo 您的电脑不能与激活服务器 %server% 连接，即将为您尝试另一个服务器
     goto ping1
 )
 
 :ping1
 cls
 title %ver% - 网络测试（请勿关闭此窗口）
-echo 正在测试您的电脑是否能够连接到Internet...
+echo 正在测试您的电脑是否能够连接到 Internet...
 ping www.baidu.com -n 1 >nul
 if %errorlevel% EQU 0 (
-    echo 您的电脑能够连接到Internet，即将为您在线激活
+    echo 您的电脑能够连接到 Internet，即将为您在线激活
 ) else (
-    echo 您的电脑不能够连接到Internet，即将为您本地激活
-    echo 您的电脑不能够连接到Internet，即将为您本地激活 >>"%systemdrive%\Windows\Setup\xrkmsini.log"
+    echo 您的电脑不能够连接到 Internet，即将为您本地激活
+    echo 您的电脑不能够连接到 Internet，即将为您本地激活 >>"%systemdrive%\Windows\Setup\xrkmsini.log"
     goto offline
 )
-echo 正在测试您的电脑是否能与激活服务器%server1%连接...
+echo 正在测试您的电脑是否能与激活服务器 %server1% 连接...
 vlmcs.exe -l 1 %server1% 2>nul | find /i "successful" 1>nul 2>nul && (
-    echo 您的电脑能与激活服务器%server1%连接，即将为您在线激活
+    echo 您的电脑能与激活服务器 %server1% 连接，即将为您在线激活
     set server=%server1%
     goto online
 )
-echo 您的电脑不能与激活服务器%server1%连接，即将为您尝试另一个服务器
+echo 您的电脑不能与激活服务器 %server1% 连接，即将为您尝试另一个服务器
 vlmcs.exe -l 1 %server2% 2>nul | find /i "successful" 1>nul 2>nul && (
-    echo 您的电脑能与激活服务器%server2%连接，即将为您在线激活
+    echo 您的电脑能与激活服务器 %server2% 连接，即将为您在线激活
     set server=%server2%
     goto online
 )
-echo 您的电脑不能与激活服务%server2%器连接，即将为您尝试另一个服务器
+echo 您的电脑不能与激活服务器 %server2% 连接，即将为您尝试另一个服务器
 vlmcs.exe -l 1 %server3% 2>nul | find /i "successful" 1>nul 2>nul && (
-    echo 您的电脑能与激活服务器%server3%连接，即将为您在线激活
+    echo 您的电脑能与激活服务器 %server3% 连接，即将为您在线激活
     set server=%server3%
     goto online
 )
-echo 您的电脑不能与激活服务%server3%器连接，即将为您尝试另一个服务器
+echo 您的电脑不能与激活服务器 %server3% 连接，即将为您尝试另一个服务器
 vlmcs.exe -l 1 %server4% 2>nul | find /i "successful" 1>nul 2>nul && (
-    echo 您的电脑能与激活服务器%server4%连接，即将为您在线激活
+    echo 您的电脑能与激活服务器 %server4% 连接，即将为您在线激活
     set server=%server4%
     goto online
 )
-echo 您的电脑不能与激活服务%server5%器连接，即将为您尝试另一个服务器
+echo 您的电脑不能与激活服务器 %server5% 连接，即将为您尝试另一个服务器
 vlmcs.exe -l 1 %server5% 2>nul | find /i "successful" 1>nul 2>nul && (
-    echo 您的电脑能与激活服务器%server5%连接，即将为您在线激活
+    echo 您的电脑能与激活服务器 %server5% 连接，即将为您在线激活
     set server=%server5%
     goto online
 )
@@ -205,20 +198,20 @@ exit
 
 :windowsentg
 if %osver% LEQ 3 goto windowsact
-echo 正在获取当前的Windows SKU...
-echo 正在获取当前的Windows SKU... >>"%systemdrive%\Windows\Setup\xrkmsini.log"
+echo 正在获取当前的 Windows SKU...
+echo 正在获取当前的 Windows SKU... >>"%systemdrive%\Windows\Setup\xrkmsini.log"
 for /F "tokens=3* delims=: " %%A in ('dism /english /online /Get-CurrentEdition ^| find /i "Current Edition :"') do set "WIN_SKU=%%A"
-echo 当前的Windows SKU为：%WIN_SKU%
-echo 当前的Windows SKU为：%WIN_SKU% >>"%systemdrive%\Windows\Setup\xrkmsini.log"
+echo 当前的 Windows SKU 为：%WIN_SKU%
+echo 当前的 Windows SKU 为：%WIN_SKU% >>"%systemdrive%\Windows\Setup\xrkmsini.log"
 echo 正在检查是否需要转换...
 echo 正在检查是否需要转换... >>"%systemdrive%\Windows\Setup\xrkmsini.log"
 if "%WIN_SKU%"=="EnterpriseG" (
-    echo 当前的Windows已经是EnterpriseG SKU，无需转换。
-    echo 当前的Windows已经是EnterpriseG SKU，无需转换。 >>"%systemdrive%\Windows\Setup\xrkmsini.log"
+    echo 当前的 Windows 已经是 EnterpriseG SKU，无需转换。
+    echo 当前的 Windows 已经是 EnterpriseG SKU，无需转换。 >>"%systemdrive%\Windows\Setup\xrkmsini.log"
 ) else (
     cd /d "%~dp0"
-    echo 正在进行SKU转换...
-    echo 正在进行SKU转换... >>"%systemdrive%\Windows\Setup\xrkmsini.log"
+    echo 正在进行 SKU 转换...
+    echo 正在进行 SKU 转换... >>"%systemdrive%\Windows\Setup\xrkmsini.log"
     if not exist "%windir%\System32\spp\tokens\skus\EnterpriseG-Volume-GVLK-1-ul-rtm.xrm-ms" expand -r -F:* EnterpriseG.cab "%windir%\System32\spp\tokens\skus\\" >>"%systemdrive%\Windows\Setup\xrkmsini.log"
     cscript.exe //B %windir%\System32\slmgr.vbs /rilc >>"%systemdrive%\Windows\Setup\xrkmsini.log"
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\Software Protection Platform" /v NoGenTicket /t REG_DWORD /d 1 /f 1>nul 2>nul
